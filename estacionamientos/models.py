63adebf56137db1975305c4cfd0eb7a366308838
andrea <andreacent8@gmail.com>
2015-05-13 07:49:08 -0430

    Casos bordes Propietario

--- estacionamientos/models.py
+++ estacionamientos/models.py
@@ -9,7 +9,7 @@ from datetime import timedelta
 class Propietario(models.Model):
 	nombres     = models.CharField(max_length = 50)
 	apellidos   = models.CharField(max_length = 50)
-	cedula      = models.CharField(max_length = 12)
+	cedula      = models.CharField(max_length = 12,unique=True)
 
 	def __str__(self):
 		return self.nombres+' '+str(self.cedula)
@@ -17,7 +17,7 @@ class Propietario(models.Model):
 class Estacionamiento(models.Model):
 	nombre      = models.CharField(max_length = 50)
 	direccion   = models.TextField(max_length = 120)
-	rif         = models.CharField(max_length = 12)
+	rif         = models.CharField(max_length = 12,unique=True)
 	telefono1   = models.CharField(blank = True, null = True, max_length = 30)
 	telefono2   = models.CharField(blank = True, null = True, max_length = 30)
 	telefono3   = models.CharField(blank = True, null = True, max_length = 30)
--- tests/tests_propietario_form.py
+++ tests/tests_propietario_form.py
@@ -11,6 +11,36 @@ from estacionamientos.forms import PropietarioForm
 
 class PropietarioAllFormTestCase(TestCase):
     
+    # caso borde
+    def test_Propietario_nombres_vacio(self):
+        form_data = {
+            'nombres': '',
+            'apellidos': 'Perez',
+            'cedula': '24981045'
+        }
+        form = PropietarioForm(data = form_data)
+        self.assertFalse(form.is_valid())
+        
+    # caso borde
+    def test_Propietario_apellidos_vacio(self):
+        form_data = {
+            'nombres': 'Carlos',
+            'apellidos': '',
+            'cedula': '24981045'
+        }
+        form = PropietarioForm(data = form_data)
+        self.assertFalse(form.is_valid())
+        
+    # caso borde
+    def test_Propietario_cedula_vacio(self):
+        form_data = {
+            'nombres': 'Carlos',
+            'apellidos': 'Perez',
+            'cedula': ''
+        }
+        form = PropietarioForm(data = form_data)
+        self.assertFalse(form.is_valid())
+        
     # malicia
     def test_Propietario_invalido_digitos_en_campo(self):
         form_data = {
@@ -21,6 +51,16 @@ class PropietarioAllFormTestCase(TestCase):
         form = PropietarioForm(data = form_data)
         self.assertFalse(form.is_valid())
 
+    # malicia
+    def test_Propietario_invalido_campo_cedula(self):
+        form_data = {
+            'nombres': '12345',
+            'apellidos': 'Perez',
+            'cedula': '2A-981045'
+        }
+        form = PropietarioForm(data = form_data)
+        self.assertFalse(form.is_valid())
+        
     # malicia
     def test_Propietario_invalido_simbolos_especiales(self):
         form_data = {
