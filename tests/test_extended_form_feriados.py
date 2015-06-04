88357f245e389941769cd33c04b4f19eb36b3c34
andrea <andreacent8@gmail.com>
2015-06-04 15:22:25 -0430

    cambio a tdd

--- tests/test_extended_form_feriados.py
+++ tests/test_extended_form_feriados.py
@@ -4,7 +4,9 @@ from django.test import TestCase
 
 from datetime import time
 
-from estacionamientos.forms import EstacionamientoExtendedForm
+from estacionamientos.models import Propietario,Estacionamiento
+
+from estacionamientos.forms import EstacionamientoExtendedForm,PropietarioForm,EstacionamientoForm,CedulaForm
 
 ###################################################################
 # ESTACIONAMIENTO_EXTENDED_FORM CON ESQUEMA PARA DIAS FERIADOS
@@ -19,7 +21,7 @@ from estacionamientos.forms import EstacionamientoExtendedForm
     #2015-12-25
 
 class ExtendedFormDiasFeriadosTestCase(TestCase):
-	
+
     # tdd
     def test_estacionamiento_extended_esquema_dia_feriados_igual(self):
         form_data = { 'puestos': 10,
@@ -41,15 +43,3 @@ class ExtendedFormDiasFeriadosTestCase(TestCase):
                       'esquemaFeriados':'TarifaMinuto'}
         form = EstacionamientoExtendedForm(data = form_data)
         self.assertTrue(form.is_valid())
-
-    # tdd
-    def test_estacionamiento_extended_dias_feriados_default(self):
-        form_data = { 'puestos': 10,
-                      'horarioin': time(hour = 6,  minute = 0),
-                      'horarioout': time(hour = 19,  minute = 0),
-                      'tarifa': '12',
-                      'esquema':'TarifaHora',
-                      'esquemaFeriados':'TarifaMinuto'}
-        form = EstacionamientoExtendedForm(data = form_data)
-        feriados = form.feriados.split(',')
-        self.assertTrue('2015-06-24' in feriados)
