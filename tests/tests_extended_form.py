58bf95cc58a611b9f5e7e639c736751431ed94bb
andreacent <andreacent8@gmail.com>
2015-06-07 12:31:32 -0430

    agregando los campos puestos_C, puestos_M y puestos_D

--- tests/tests_extended_form.py
+++ tests/tests_extended_form.py
@@ -44,12 +44,51 @@ class ExtendedFormTestCase(TestCase):
         self.assertFalse(form.is_valid())
 
     # caso borde
-    def test_estacionamiento_extended_form_todos_campos_bien(self):
+    def test_estacionamiento_extended_form_cinco_bien(self):
         form_data = { 'puestos': 2,
                       'horarioin': time(hour = 6,  minute = 0),
                       'horarioout': time(hour = 19,  minute = 0),
                       'tarifa': '12',
                       'esquema':'TarifaMinuto'
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
+
+    # tdd
+    def test_estacionamiento_extended_form_seis_bien(self):
+        form_data = { 'puestos': 2,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaMinuto',
+                      'puestos_C': 2
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
+
+    # tdd
+    def test_estacionamiento_extended_form_siete_bien(self):
+        form_data = { 'puestos': 2,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaMinuto',
+                      'puestos_C': 2,
+                      'puestos_M': 2
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
+
+    # caso borde
+    def test_estacionamiento_extended_form_todos_campos_bien(self):
+        form_data = { 'puestos': 2,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaMinuto',
+                      'puestos_C': 2,
+                      'puestos_M': 2,
+                      'puestos_D': 2
                     }
         form = EstacionamientoExtendedForm(data = form_data)
         self.assertTrue(form.is_valid())
@@ -75,6 +114,66 @@ class ExtendedFormTestCase(TestCase):
         self.assertFalse(form.is_valid())
 
     # caso borde
+    def test_estacionamiento_extended_form_puestos_camiones_1(self):
+        form_data = { 'puestos_C': 1,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'}
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
+
+    # caso borde
+    def test_estacionamiento_extended_form_puestos_camiones_0(self):
+        form_data = { 'puestos_C': 0,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'}
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertFalse(form.is_valid())
+
+    # caso borde
+    def test_estacionamiento_extended_form_puestos_discapacitados_1(self):
+        form_data = { 'puestos_D': 1,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'}
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
+
+    # caso borde
+    def test_estacionamiento_extended_form_puestos_discapacitados_0(self):
+        form_data = { 'puestos_D': 0,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'}
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertFalse(form.is_valid())
+
+    # caso borde
+    def test_estacionamiento_extended_form_puestos_motos_1(self):
+        form_data = { 'puestos_M': 1,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'}
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertTrue(form.is_valid())
+
+    # caso borde
+    def test_estacionamiento_extended_form_puestos_motos_0(self):
+        form_data = { 'puestos_M': 0,
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'}
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertFalse(form.is_valid())
+
+    # caso borde
     def test_estacionamiento_extended_form_hora_inicio_igual_hora_cierre(self):
         form_data = { 'puestos': 2,
                       'horarioin': time(hour = 6,  minute = 0),
@@ -88,6 +187,39 @@ class ExtendedFormTestCase(TestCase):
     # malicia
     def test_estacionamiento_extended_form_string_en_campo_puesto(self):
         form_data = { 'puestos': 'hola',
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertFalse(form.is_valid())
+
+    # malicia
+    def test_estacionamiento_extended_form_string_en_campo_puesto_C(self):
+        form_data = { 'puestos_C': 'hola',
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertFalse(form.is_valid())
+
+    # malicia
+    def test_estacionamiento_extended_form_string_en_campo_puesto_D(self):
+        form_data = { 'puestos_D': 'hola',
+                      'horarioin': time(hour = 6,  minute = 0),
+                      'horarioout': time(hour = 19,  minute = 0),
+                      'tarifa': '12',
+                      'esquema':'TarifaHora'
+                    }
+        form = EstacionamientoExtendedForm(data = form_data)
+        self.assertFalse(form.is_valid())
+
+    # malicia
+    def test_estacionamiento_extended_form_string_en_campo_puesto_M(self):
+        form_data = { 'puestos_M': 'hola',
                       'horarioin': time(hour = 6,  minute = 0),
                       'horarioout': time(hour = 19,  minute = 0),
                       'tarifa': '12',
