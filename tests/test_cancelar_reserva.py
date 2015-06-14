7901d5e1462433fd84f12f196869aa12c7210dc3
Carlos Noria <nucinm@gmail.com>
2015-06-14 16:34:49 -0430

    Terminado los test de pago por cancelar

--- tests/test_cancelar_reserva.py
+++ tests/test_cancelar_reserva.py
@@ -19,7 +19,7 @@ from estacionamientos.models import (
     Recargas,
     Cancelaciones,
     Propietario,
-    Estacionamiento)
+    Estacionamiento, AdministracionSage)
 
 
 def crear_propietario():
@@ -61,30 +61,31 @@ def crear_billetera(monto = 0):
     )
     r.save()
     
-def crear_factura(_reserva, monto):
+def crear_factura(_reserva, monto, tarjetaTipo, id_billetera):
     pago = Pago(
             reserva = _reserva,
             id = asigna_id_unico(),
             fechaTransaccion = datetime.now(),
             cedula = "10",
             cedulaTipo = "V",
-            tarjetaTipo = "Vista",
-            monto = Decimal(monto)
+            tarjetaTipo = tarjetaTipo,
+            monto = Decimal(monto),
+            idBilletera = id_billetera
     )
     pago.save()
     
-def crear_pago(h_inicio, h_fin, monto):
+def crear_pago(h_inicio, h_fin, monto, tarjetaTipo = 'Vista', id_billetera = None):
     p = crear_propietario()
     e = crear_estacionamiento(p)
     r = crear_reserva(h_inicio, h_fin, e)
-    crear_factura(r, monto)
+    crear_factura(r, monto, tarjetaTipo, id_billetera)
 
-def cancelar_reservacion(id_pago, id_billetera, tiempo = datetime.now() + timedelta(seconds = 60), monto_debitar = 0):
+def cancelar_reservacion(id_pago, id_billetera, monto_debitar, tiempo = datetime.now() + timedelta(seconds = 60)):
     try:
         pago = Pago.objects.get(pk = id_pago)
         billetera = BilleteraElectronica.objects.get(pk = id_billetera)
         if ((pago.validar_cancelacion(tiempo)) and 
-            (billetera.validar_recarga(pago.monto))):
+            (billetera.validar_recarga(pago.monto - monto_debitar))):
             c = Cancelaciones(
                   pagoCancelado = pago,
                   billetera = billetera,
@@ -93,14 +94,12 @@ def cancelar_reservacion(id_pago, id_billetera, tiempo = datetime.now() + timede
                   fechaTransaccion = datetime.now()            
             )
             pago.cancelar_reserva()
-            billetera.recargar_saldo(pago.monto)
+            billetera.recargar_saldo(pago.monto - monto_debitar)
             c.save()
+
     except:
         pass
-        
-    
-       
-        
+                
 class TestCancelarReserva(TestCase):
     
     # borde
@@ -108,9 +107,9 @@ class TestCancelarReserva(TestCase):
         inicio = datetime.now() + timedelta(days = 1)
         fin = datetime.now() + timedelta(days = 1, hours = 1)
         monto = Decimal('0.01')
-        crear_pago(inicio, fin, monto)
         crear_billetera()
-        cancelar_reservacion(1, 1)
+        crear_pago(inicio, fin, monto, 'Billetera Electronica', 1)
+        cancelar_reservacion(1, 1, monto_debitar = 0)
         p = Pago.objects.get(pk = 1)
         b = BilleteraElectronica.objects.get(pk = 1)
         self.assertTrue(p.cancelado and b.saldo == monto)
@@ -120,22 +119,31 @@ class TestCancelarReserva(TestCase):
         inicio = datetime.now() + timedelta(days = 1)
         fin = datetime.now() + timedelta(days = 1, hours = 1)
         monto = 0
-        crear_pago(inicio, fin, monto)
+        crear_pago(inicio, fin, monto, 'Billetera Electronica', 1)
         crear_billetera()
-        cancelar_reservacion(1, 1)
+        cancelar_reservacion(1, 1, monto_debitar = 0)
         self.assertEqual(len(Cancelaciones.objects.all()), 0)
         
     # borde
     def testCancelarReservacionMontoMaximo(self):
+        AdministracionSage.objects.create_AdministracionSage(9.9)
         inicio = datetime.now() + timedelta(days = 1)
         fin = datetime.now() + timedelta(days = 1, hours = 1)
         monto = 10000
-        crear_pago(inicio, fin, monto)
+        crear_pago(inicio, fin, monto, 'Vista')
         crear_billetera()
-        cancelar_reservacion(1, 1)
+        admin = AdministracionSage.objects.get(pk = 1)
+        p = Pago.objects.get(pk = 1)
+        aplicaCargo = p.factura_inicial_pagada_billetera()
+        if aplicaCargo:
+            monto_debitar = admin.calcular_monto(monto)
+            
+        else:
+            monto_debitar = 0
+        cancelar_reservacion(1, 1, monto_debitar)
         p = Pago.objects.get(pk = 1)
         b = BilleteraElectronica.objects.get(pk = 1)
-        self.assertTrue(p.cancelado and b.saldo == monto)
+        self.assertTrue(p. cancelado and b.saldo == 9010)
         
     # borde
     def testCancelarReservacionBilleteraFull(self):
@@ -144,7 +152,7 @@ class TestCancelarReserva(TestCase):
         monto = Decimal('0.01')
         crear_pago(inicio, fin, monto)
         crear_billetera(10000)
-        cancelar_reservacion(1, 1)
+        cancelar_reservacion(1, 1, monto_debitar = 0)
         cancelaciones = Cancelaciones.objects.all()
         self.assertEqual(len(cancelaciones), 0)
       
@@ -155,15 +163,15 @@ class TestCancelarReserva(TestCase):
         monto = 50
         crear_pago(inicio, fin, monto)
         crear_billetera()
-        cancelar_reservacion(1, 1)
-        cancelar_reservacion(1, 1)
+        cancelar_reservacion(1, 1, monto_debitar = 0)
+        cancelar_reservacion(1, 1, monto_debitar = 0)
         cancelaciones = Cancelaciones.objects.all()
         self.assertEqual(len(cancelaciones), 1)
         
     # malicia
     def TestCancelarReservaNoExiste(self):
         crear_billetera()
-        cancelar_reservacion(1, 1)
+        cancelar_reservacion(1, 1, 0)
         cancelaciones = Cancelaciones.objects.all()
         self.assertEqual(len(cancelaciones), 0)
     
@@ -174,7 +182,7 @@ class TestCancelarReserva(TestCase):
         fin = datetime.now() + timedelta(days = 1, hours = 1)
         monto = Decimal('0.01')
         crear_pago(inicio, fin, monto)
-        cancelar_reservacion(1, 1)
+        cancelar_reservacion(1, 1, 0)
         cancelaciones = Cancelaciones.objects.all()
         self.assertEqual(len(cancelaciones), 0)
         
@@ -185,7 +193,7 @@ class TestCancelarReserva(TestCase):
         monto = Decimal('0.01')
         crear_pago(inicio, fin, monto)
         crear_billetera(10000)
-        cancelar_reservacion(1, 1, datetime(2015, 6, 4, 8, 4, 0, 0))
+        cancelar_reservacion(1, 1, 0, datetime(2015, 6, 4, 8, 4, 0, 0))
         cancelaciones = Cancelaciones.objects.all()
         self.assertEqual(len(cancelaciones), 1)
         
@@ -196,6 +204,48 @@ class TestCancelarReserva(TestCase):
         monto = Decimal('0.01')
         crear_pago(inicio, fin, monto)
         crear_billetera(10000)
-        cancelar_reservacion(1, 1, datetime(2015, 6, 4, 8, 5, 0, 0))
+        cancelar_reservacion(1, 1, 0, datetime(2015, 6, 4, 8, 5, 0, 0))
         cancelaciones = Cancelaciones.objects.all()
         self.assertEqual(len(cancelaciones), 0)
+    
+    # interno
+    def testCancelarReservaPagadaBilletera(self):
+        AdministracionSage.objects.create_AdministracionSage(9.9)
+        inicio = datetime.now() + timedelta(days = 1)
+        fin = datetime.now() + timedelta(days = 1, hours = 1)
+        monto = 100
+        crear_billetera()
+        crear_pago(inicio, fin, monto, 'Billetera Electronica', 1)
+        admin = AdministracionSage.objects.get(pk = 1)
+        p = Pago.objects.get(pk = 1)
+        aplicaCargo = p.factura_inicial_pagada_billetera()
+        if aplicaCargo:
+            monto_debitar = admin.calcular_monto(monto)
+            
+        else:
+            monto_debitar = 0
+        cancelar_reservacion(1, 1, monto_debitar)
+        p = Pago.objects.get(pk = 1)
+        b = BilleteraElectronica.objects.get(pk = 1)
+        self.assertTrue(p. cancelado and b.saldo == 100)
+        
+    # interno
+    def testCancelarReservaPagadaTarjetaCredito(self):
+        AdministracionSage.objects.create_AdministracionSage(9.9)
+        inicio = datetime.now() + timedelta(days = 1)
+        fin = datetime.now() + timedelta(days = 1, hours = 1)
+        monto = 100
+        crear_billetera()
+        crear_pago(inicio, fin, monto, 'Vista', 1)
+        admin = AdministracionSage.objects.get(pk = 1)
+        p = Pago.objects.get(pk = 1)
+        aplicaCargo = p.factura_inicial_pagada_billetera()
+        if aplicaCargo:
+            monto_debitar = admin.calcular_monto(monto)
+            
+        else:
+            monto_debitar = 0
+        cancelar_reservacion(1, 1, monto_debitar)
+        p = Pago.objects.get(pk = 1)
+        b = BilleteraElectronica.objects.get(pk = 1)
+        self.assertTrue(p. cancelado and b.saldo == Decimal('90.1'))
