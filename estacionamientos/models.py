2dfbb7494ef6c44a51f0c2aa1e016eb379e3ed62
andrea <andreacent8@gmail.com>
2015-06-15 09:52:51 -0430

    separadas funciones de calcularTarifa

--- estacionamientos/models.py
+++ estacionamientos/models.py
@@ -253,33 +253,40 @@ class EsquemaTarifario(models.Model):
     tarifa_D         = models.DecimalField(blank = True, null = True, max_digits=20, decimal_places=2)
     tarifa2_D        = models.DecimalField(blank = True, null = True, max_digits=10, decimal_places=2)
 
-    #devuelve las tarifas segun el tipo de vehiculo
-    def obtenerTarifa(self, tipoDeVehiculo):
+    #devuelve las tarifas regulares segun el tipo de vehiculo
+    def obtenerTarifa1(self, tipoDeVehiculo):
         tarifa  = 0
-        tarifa2 = 0
         if tipoDeVehiculo == "Particular":
             tarifa  = tarifa
-            tarifa2 = tarifa2
         elif tipoDeVehiculo == "Moto":
             tarifa  = tarifa_M
-            tarifa2 = tarifa2_M
         elif tipoDeVehiculo == "Camion":
             tarifa  = tarifa_C
-            tarifa2 = tarifa2_C
         elif tipoDeVehiculo == "Discapacitado":
             tarifa  = tarifa_D
-            tarifa2 = tarifa2_D
-        return (tarifa,tarifa2) 
+        return tarifa 
 
+    #devuelve las tarifas segun el tipo de vehiculo
+    def obtenerTarifa2(self, tipoDeVehiculo):
+        tarifa2 = 0
+        if tipoDeVehiculo == "Particular":
+            tarifa2 = tarifa2
+        elif tipoDeVehiculo == "Moto":
+            tarifa2 = tarifa2_M
+        elif tipoDeVehiculo == "Camion":
+            tarifa2 = tarifa2_C
+        elif tipoDeVehiculo == "Discapacitado":
+            tarifa2 = tarifa2_D
+        return tarifa2
+   
     class Meta:
         abstract = True
     def __str__(self):
         return str(self.tarifa)
 
-
 class TarifaHora(EsquemaTarifario):
-    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo):
-        tarifa,tarifa2 = self.obtenerTarifa(tipoDeVehiculo)
+    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo='Particular'):
+        tarifa = self.obtenerTarifa1(tipoDeVehiculo)
         a = horaFinal-horaInicio
         a = a.days*24+a.seconds/3600
         a = ceil(a) #  De las horas se calcula el techo de ellas
@@ -289,8 +296,8 @@ class TarifaHora(EsquemaTarifario):
         return("Por Hora")
 
 class TarifaMinuto(EsquemaTarifario):
-    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo):
-        tarifa,tarifa2 = self.obtenerTarifa(tipoDeVehiculo)
+    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo='Particular'):
+        tarifa = self.obtenerTarifa1(tipoDeVehiculo)
         minutes = horaFinal-horaInicio
         minutes = minutes.days*24*60+minutes.seconds/60
         return (Decimal(minutes)*Decimal(tarifa/60)).quantize(Decimal('1.00'))
@@ -298,8 +305,8 @@ class TarifaMinuto(EsquemaTarifario):
         return("Por Minuto")
 
 class TarifaHorayFraccion(EsquemaTarifario):
-    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo):
-        tarifa,tarifa2 = self.obtenerTarifa(tipoDeVehiculo)
+    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo='Particular'):
+        tarifa = self.obtenerTarifa1(tipoDeVehiculo)
         time = horaFinal-horaInicio
         time = time.days*24*3600+time.seconds
         if(time>3600):
@@ -318,8 +325,9 @@ class TarifaHorayFraccion(EsquemaTarifario):
         return("Por Hora y Fraccion")
 
 class TarifaFinDeSemana(EsquemaTarifario):
-    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo):
-        tarifa,tarifa2 = self.obtenerTarifa(tipoDeVehiculo)
+    def calcularPrecio(self,horaInicio,horaFinal,tipoDeVehiculo='Particular'):
+        tarifa = self.obtenerTarifa1(tipoDeVehiculo)
+        tarifa2 = self.obtenerTarifa2(tipoDeVehiculo)
         minutosNormales    = 0
         minutosFinDeSemana = 0
         tiempoActual       = inicio
@@ -345,8 +353,9 @@ class TarifaFinDeSemana(EsquemaTarifario):
         return("Tarifa diferenciada para fines de semana")
 
 class TarifaHoraPico(EsquemaTarifario):
-    def calcularPrecio(self,reservaInicio,reservaFinal,tipoDeVehiculo):
-        tarifa,tarifa2 = self.obtenerTarifa(tipoDeVehiculo)
+    def calcularPrecio(self,reservaInicio,reservaFinal,tipoDeVehiculo='Particular'):
+        tarifa = self.obtenerTarifa1(tipoDeVehiculo)
+        tarifa2 = self.obtenerTarifa2(tipoDeVehiculo)
         minutosPico  = 0
         minutosValle = 0
         tiempoActual = reservaInicio
