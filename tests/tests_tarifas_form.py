eb0cb746ee1e735c91f9e9cadcaf1121dfd5caef
andrea <andreacent8@gmail.com>
2015-06-16 13:05:01 -0430

    modificados casos de pruebas para formulario de tarifas

--- tests/tests_tarifas_form.py
+++ tests/tests_tarifas_form.py
@@ -18,26 +18,7 @@ class TarifasFormTestCase(TestCase):
     #un solo campo (sin el obligatorio)
     def test_Un_Campo_No_Obligatorio(self):
         form_data = {
-            'tarifaFeriados': 9
-        }
-        form = TarifasForm(data = form_data)
-        self.assertFalse(form.is_valid())
-
-    #dos campos (sin el obligatorio) 
-    def test_Dos_Campos_No_Obligatorios(self):
-        form_data = {
-            'tarifa2': 10,
-            'tarifaFeriados': 9
-        }
-        form = TarifasForm(data = form_data)
-        self.assertFalse(form.is_valid())
-
-    #tres campos (sin el obligatorio) 
-    def test_Tres_Campos_No_Obligatorios(self):
-        form_data = {
-            'tarifa2': 20,
-            'tarifaFeriados': 9,
-            'tarifaFeriados2': 10
+            'tarifa2': 9
         }
         form = TarifasForm(data = form_data)
         self.assertFalse(form.is_valid())
@@ -46,9 +27,7 @@ class TarifasFormTestCase(TestCase):
     def test_Todos_Los_Campos(self):
         form_data = {
         	'tarifa': 20,
-            'tarifa2': 20,
-            'tarifaFeriados': 9,
-            'tarifaFeriados2': 10
+            'tarifa2': 10
         }
         form = TarifasForm(data = form_data)
         self.assertTrue(form.is_valid())
@@ -69,9 +48,7 @@ class TarifasFormTestCase(TestCase):
     def test_Tarifa_igual_cero(self):
         form_data = {
             'tarifa': 0,
-            'tarifa2': 20,
-            'tarifaFeriados': 9,
-            'tarifaFeriados2': 10
+            'tarifa2': 20
         }
         form = TarifasForm(data = form_data)
         self.assertTrue(form.is_valid())
@@ -80,31 +57,7 @@ class TarifasFormTestCase(TestCase):
     def test_Tarifa_Especial_igual_cero(self):
         form_data = {
             'tarifa': 20,
-            'tarifa2': 0,
-            'tarifaFeriados': 9,
-            'tarifaFeriados2': 10
-        }
-        form = TarifasForm(data = form_data)
-        self.assertTrue(form.is_valid())
-
-    #tarifaFeriados = 0
-    def test_Tarifa_Feriada_igual_cero(self):
-        form_data = {
-            'tarifa': 20,
-            'tarifa2': 30,
-            'tarifaFeriados': 0,
-            'tarifaFeriados2': 10
-        }
-        form = TarifasForm(data = form_data)
-        self.assertTrue(form.is_valid())
-
-    #tarifaFeriados2 = 0
-    def test_Tarifa_Feriada_Especial_igual_cero(self):
-        form_data = {
-            'tarifa': 20,
-            'tarifa2': 30,
-            'tarifaFeriados': 10,
-            'tarifaFeriados2': 0
+            'tarifa2': 0
         }
         form = TarifasForm(data = form_data)
         self.assertTrue(form.is_valid())
@@ -114,12 +67,10 @@ class TarifasFormTestCase(TestCase):
     #########################
 
     #tarifas feriadas iguales a cero
-    def test_Tarifas_Feriadas_iguales_cero(self):
+    def test_Tarifas_iguales_cero(self):
         form_data = {
-            'tarifa': 20,
-            'tarifa2': 30,
-            'tarifaFeriados': 0,
-            'tarifaFeriados2': 0
+            'tarifa': 0,
+            'tarifa2': 0
         }
         form = TarifasForm(data = form_data)
         self.assertTrue(form.is_valid())
@@ -128,24 +79,11 @@ class TarifasFormTestCase(TestCase):
     # MALICIA
     #########################
 
-    #todas las tarifas igual a cero
-    def test_tarifas_iguales_a_cero(self):
-        form_data = {
-            'tarifa': 0,
-            'tarifa2': 0,
-            'tarifaFeriados': 0,
-            'tarifaFeriados2': 0
-        }
-        form = TarifasForm(data = form_data)
-        self.assertTrue(form.is_valid())
-
     #letras en el primer campo
     def test_tarifa_letras(self):
         form_data = {
             'tarifa': 'hola',
-            'tarifa2': 10,
-            'tarifaFeriados': 20,
-            'tarifaFeriados2': 30
+            'tarifa2': 10
         }
         form = TarifasForm(data = form_data)
         self.assertFalse(form.is_valid())
@@ -154,31 +92,7 @@ class TarifasFormTestCase(TestCase):
     def test_tarifa2_letras(self):
         form_data = {
             'tarifa': 20,
-            'tarifa2': 'hola',
-            'tarifaFeriados': 30,
-            'tarifaFeriados2': 10
-        }
-        form = TarifasForm(data = form_data)
-        self.assertFalse(form.is_valid())
-
-    #letras en el tercer campo
-    def test_tarifaFeriados_letras(self):
-        form_data = {
-            'tarifa': 20,
-            'tarifa2': 30,
-            'tarifaFeriados': 'hola',
-            'tarifaFeriados2': 10
-        }
-        form = TarifasForm(data = form_data)
-        self.assertFalse(form.is_valid())
-
-    #letras en el cuarto campo
-    def test_tarifaFeriados4_letras(self):
-        form_data = {
-            'tarifa': 20,
-            'tarifa2': 30,
-            'tarifaFeriados': 10,
-            'tarifaFeriados2': 'hola'
+            'tarifa2': 'hola'
         }
         form = TarifasForm(data = form_data)
         self.assertFalse(form.is_valid())
